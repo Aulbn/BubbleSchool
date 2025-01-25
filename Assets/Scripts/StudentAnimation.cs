11f26cc1e2edc57a04040528686d3547c294ab5d
Guest Person <gst15813@hs.local>
2025-01-25 17:48:56 +0100

    Student base model and animation

---
+++ Assets/Scripts/StudentAnimation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudentAnimation : MonoBehaviour
+{
+
+    public Animator StudentAnimator;
+    private float _IdleOffset;
+
+    void Start()
+    {
+        _IdleOffset = Random.Range(0f, 1f);
+
+        StudentAnimator.SetFloat("IdleOffset", _IdleOffset);
+
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
